b984d0fe628f19bdd31df22dbfe1d7b7e8e0cf14
Don Benny <donbenny916@gmail.com>
2023-10-04 09:18:15 +0530

    requests url

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import cv2
 import pytesseract
 import numpy as np  
 import tensorflow as tf 
+import requests
+from io import BytesIO
+
 # Set Tesseract path
-# pytesseract.pytesseract.tesseract_cmd = r'F:\SDKs\pytesseract\tesseract.exe'
 pytesseract.pytesseract.tesseract_cmd = 'pytesseract/tesseract.exe'
 
 app = Flask(__name__)
@@ -16,18 +18,18 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-def preprocess_image(image_path):
-    img = cv2.imread(image_path)
+def preprocess_image_from_url(image_url):
+    response = requests.get(image_url)
+    img = cv2.imdecode(np.frombuffer(response.content, np.uint8), cv2.IMREAD_COLOR)
     img = cv2.resize(img, (224, 224))
     img = img / 255.0
-    img = np.float32(img)  # Convert to FLOAT32
-    img = np.expand_dims(img, axis=0)  # Add an extra dimension for batch size
+    img = np.float32(img)
+    img = np.expand_dims(img, axis=0)
     return img
 
-
-def predictByImage(input_image_path):
+def predictByImageFromURL(image_url):
     # Preprocess the input image
-    input_image = preprocess_image(input_image_path)
+    input_image = preprocess_image_from_url(image_url)
     class_labels = ["Aadhaar", "PAN", "Driving Licence", "Voter ID", "Passport", "Utility", "credit_cards"]
 
     # Run the inference
@@ -38,7 +40,8 @@ def predictByImage(input_image_path):
     predicted_class_index = np.argmax(predictions)
     predicted_class = class_labels[predicted_class_index]
     if predicted_class == "Aadhaar":
-        img = cv2.imread(input_image_path)
+        response = requests.get(image_url)
+        img = cv2.imdecode(np.frombuffer(response.content, np.uint8), cv2.IMREAD_COLOR)
         image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         text = pytesseract.image_to_string(image_rgb)
         return jsonify({"predicted_class": predicted_class, "data": text})
@@ -49,13 +52,14 @@ def predictByImage(input_image_path):
 def predict():
     if request.method == 'POST':
         data = request.get_json()
-        input_image_path = data.get('image_path')
-        return predictByImage(input_image_path)
+        image_url = data.get('image_url')
+        return predictByImageFromURL(image_url)
     elif request.method == 'GET':
-        input_image_path = request.args.get('image_path')
-        return predictByImage(input_image_path)
+        image_url = request.args.get('image_url')
+        return predictByImageFromURL(image_url)
     else:
         return jsonify({"error": "Invalid request method"}), 405
+
 
 if __name__ == '__main__':
     app.run(debug=True)
